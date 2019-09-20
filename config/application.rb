cc288e76acf4898b76817b938572a377b03f5030
umair raza <umair.raza101@gmail.com>
2019-09-20 14:25:43 +0500

    make it better

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
-	config.autoload_paths += %W(#{config.root}/services)
+	
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
